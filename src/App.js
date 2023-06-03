95864e595db79c4a59488d3d270437b0e5b05df5
xYerMikx <yermik2014@yandex.ru>
2023-06-03 22:35:25 +0300

    feat: make counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './index.scss';
+import "./index.scss";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={decrement} className="minus">- Минус</button>
+        <button onClick={increment} className="plus">Плюс +</button>
       </div>
     </div>
   );
